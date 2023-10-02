f6db8c84638b4429f60810da95dabb03dc95b406
Kayla de Vivanco <kayla.devivanco@utec.edu.pe>
2023-10-02 17:32:52 -0500

    sett

--- src/main/java/com/example/dbphack1/model/Grade.java
+++ src/main/java/com/example/dbphack1/model/Grade.java
@@ -19,4 +19,37 @@ public class Grade {
     @ManyToOne
     @JoinColumn(name = "alumno_id", nullable = false)
     private Alumno alumno;
+
+    public Grade() {
+    }
+
+    public Grade(int id, Long score, Alumno alumno) {
+        this.id = id;
+        this.score = score;
+        this.alumno = alumno;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
 }
